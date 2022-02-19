7b3d3889294defcd440e78f8d5c6535e19099c75
dinghui <243542757@qq.com>
2022-02-19 20:45:15 +0800

    double check 注释

--- pattern/src/main/java/singleton/lazy/LazySingletonV3.java
+++ pattern/src/main/java/singleton/lazy/LazySingletonV3.java
@@ -4,11 +4,17 @@ package singleton.lazy;
  * double check
  */
 public class LazySingletonV3 {
+
     private LazySingletonV3(){}
+
     private static LazySingletonV3 instance = null;
+
     public static LazySingletonV3 getInstance() {
+        //检查是否要阻塞
         if (null == instance) {
             synchronized (LazySingletonV3.class) {
+                //检查是否要创建实例
+                //两个线程有可能同时进入这里
                 if (null == instance) {
                     instance = new LazySingletonV3();
                 }
